dbefb076f10d555ab45ccd35816faacb65198164
Abdirabi11 <c.rabi33@gmail.com>
2025-01-20 14:18:13 +0300

    Update

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,4 +1,26 @@
 import React from "react";
+import carsList from "../carsList";
 export default function Cars() {
-  return <h1> Explore our Car options </h1>;
+  const carsEl = carsList.map((car) => {
+    return (
+      <div key={car.id} className="car-title">
+        <img src={car.img} />
+        <div className="car-info">
+          <h3>{car.name}</h3>
+          <p>
+            <span>Price: {car.price} /day </span>
+          </p>
+          <p>Type: {car.type}</p>
+          <p>Deposit: {car.deposit} </p>
+        </div>
+        <i className="car-category">{car.category}</i>
+      </div>
+    );
+  });
+  return (
+    <div className="car-list-container">
+      <h1>Explore our car options</h1>
+      <div className="car-list">{carsEl}</div>
+    </div>
+  );
 }
