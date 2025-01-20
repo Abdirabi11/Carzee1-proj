a1d242009395d005861647145bcd92f793aaa163
Abdirabi11 <c.rabi33@gmail.com>
2025-01-20 14:17:43 +0300

    update

---
+++ src/carsList.js
@@ -0,0 +1,40 @@
+const carsList = [
+    {
+      id: 1,
+      img: "https://images.unsplash.com/photo-1677517859847-0e750bfd13a9?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "BMW X1",
+      price: "70$",
+      type: "Automatic",
+      deposit: "100$",
+      category: "SUV"
+    },
+    {
+      id: 2,
+      img: "https://cdn.motor1.com/images/mgl/Ooz6WB/s1/bmw-i7-m70-xdrive-2023.webp",
+      name: "BMW i7",
+      price: "120$",
+      type: "Automatic",
+      deposit: "200$",
+      category: "SUV"
+    },
+    {
+      id: 3,
+      img: "https://arabam-blog.mncdn.com/wp-content/uploads/2021/12/kodiaq.jpeg",
+      name: "SKODA KODIEQ",
+      price: "100$",
+      type: "Automatic",
+      deposit: "200$",
+      category: "SUV"
+    },
+    {
+      id: 4,
+      img: "https://images.prismic.io/carwow/f3a0e97d-e723-477f-acf4-ab284ae2cb83_New+Mercedes+EQB+front+driving.png?auto=format&cs=tinysrgb&fit=crop&q=60&w=750",
+      name: "Mercedes EQB",
+      price: "200$",
+      type: "Automatic",
+      deposit: "200$",
+      category: "SUV"
+    },
+  ];
+  export default carsList;
+  
